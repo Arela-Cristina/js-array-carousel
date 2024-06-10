d27a062f093e44877f640b3fd8ae0c2a19bd50f3
Arela-Cristina <instagrandine@gmail.com>
2024-06-10 19:15:46 +0200

    created a for cycle to extrapolate each item from arrayCarousel.
    Created a variable  const container to call the container from the DOM to Javascript

---
+++ main.js
@@ -0,0 +1,14 @@
+//inseriamo il array carousel from Dom
+let arrayCarousel = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+
+//chiamiamo il container dove si trovano gli elementi del dom
+const container = document.getElementById('container');
+    console.log(container);
+
+//estrapoliamo gli elementi del array 
+for (let i = 0; i < arrayCarousel.length; i++) {
+    let itemCarousel = arrayCarousel[i];
+    console.log(itemCarousel);
+
+    
+}

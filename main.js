467f0144bcc3bf180aca93053d2b570008552e8e
Arela-Cristina <instagrandine@gmail.com>
2024-06-10 20:34:32 +0200

    Created a immagini variable to created a new element to put images inside, like a carousel. then put each one inside the container on html document.
    Add a new variable called immaginiContainer to save there, all elements under the class 'cover' which i created , this elements are all images.
    
    On the Css file, all images are under the property display none. So I added an active class which permit the image be on display block.

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ let arrayCarousel = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 
 //chiamiamo il container dove si trovano gli elementi del dom
 const container = document.getElementById('container');
-    console.log(container);
+console.log(container);
 
 //estrapoliamo gli elementi del array 
 for (let i = 0; i < arrayCarousel.length; i++) {
     let itemCarousel = arrayCarousel[i];
     console.log(itemCarousel);
 
-    
+    let immagini = document.createElement = `<img class="cover" src="${itemCarousel}" alt="Cover Stray Game">`;
+    console.log('sono immagini', immagini);
+
+    container.innerHTML += immagini;
 }
+
+//selezionamo tutti gli elementi in pagina
+const immaginiContainer = document.getElementsByClassName('cover');
+immaginiContainer[4].classList.add('active');
+console.log(immaginiContainer[0]);
